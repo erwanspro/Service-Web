8618668e892c002f638b3769b58c8ee67ec42b5a
ecatty <ewenn.catty@laposte.net>
2025-05-27 14:46:57 +0200

    app fini

--- app/src/main/java/com/example/serviceweb/MainActivity.java
+++ app/src/main/java/com/example/serviceweb/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.serviceweb;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +27,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     private TextView textViewDep;
@@ -33,14 +38,17 @@ public class MainActivity extends AppCompatActivity {
     private EditText saisiPraticien;
     private EditText saisiDepartement;
     private RequestQueue requestQueue;
+    private Spinner spinnerPraticiens;
+    private  Spinner spinnerDepartements;
 
     private void initView()
     {
-
         saisiPraticien = findViewById(R.id.saisiPraticien);
         saisiDepartement = findViewById(R.id.saisiDepartement);
         textViewDep = findViewById(R.id.textViewDep);
         textViewNom = findViewById(R.id.textViewNom);
+        spinnerPraticiens = findViewById(R.id.spinnerPraticiens);
+        spinnerDepartements = findViewById(R.id.spinnerDepartements);
         btnPraticien = findViewById(R.id.btnPraticien);
         btnDepartement = findViewById(R.id.btnDepartement);
         requestQueue = Volley.newRequestQueue(this);
@@ -70,33 +78,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String praticienStr = saisiPraticien.getText().toString().trim();
                 if (praticienStr.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Veuillez entrer un ou plusieur caractère",
-                            Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Veuillez entrer un ou plusieurs caractères", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                try{
-                    String nomPrat = Integer.toString(Integer.parseInt(praticienStr));
-                    if (nomPrat.contains("01234566789")) {
-                        Toast.makeText(MainActivity.this, "Veuillez ne pas entrer un nombre",
-                                Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                    fetchNomPraticien(nomPrat);
-                } catch (NullPointerException e) {
-                    Toast.makeText(MainActivity.this,"Erreur : Veuillez ne pas entrer un nombre.",
-                            Toast.LENGTH_SHORT).show();
+                if (!praticienStr.matches("[a-zA-Z]+")) { // Vérifie si la chaîne contient uniquement des chiffres
+                    Toast.makeText(MainActivity.this, "Veuillez entrer un nom valide.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                try {
+                    fetchNomPraticien(praticienStr);
+                } catch (NumberFormatException e) {
+                    Toast.makeText(MainActivity.this, "Erreur : Veuillez ne pas entrer un nombre.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
     }
-    public TextView fetchNumDep(int numdep){
+    public Spinner fetchNumDep(int numdep){
         String url = "https://gsb.siochaptalqper.fr/praticiens/numdep/" + numdep ;
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
                 Request.Method.GET, url, null,
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
+                        ArrayList<String> praticiensList = new ArrayList<>();
+                        ArrayList<String> praticiensDetails = new ArrayList<>(); // Pour stocker les détails
+
                         for (int i = 0; i < response.length(); i++) {
                             try {
                                 JSONObject jsonObject = response.getJSONObject(i);
@@ -108,12 +114,37 @@ public class MainActivity extends AppCompatActivity {
                                 String ville = jsonObject.optString("PRA_VILLE", "Inconnue");
                                 String libelle = jsonObject.optString("TYP_LIBELLE", "Inconnue");
                                 String notoriete = jsonObject.optString("PRA_COEFNOTORIETE", "Inconnue");
-                                textViewDep.append("\n" + "Numéro : " + num + "\n" + "Nom : " + nom + "\n" + "Prénom : " + prenom + "\n" + "Adresse : " + add + "\n" + "Code postal : " + cp+ "\n" + "Ville : " + ville + "\n" + "Libelle :" + libelle + "\n" + "Notoriété : " + notoriete + "\n");
+
+                                // Stocker les détails du praticien
+                                String details = "Numéro : " + num + "\nNom : " + nom + "\nPrénom : " + prenom +
+                                        "\nAdresse : " + add + "\nCode postal : " + cp +
+                                        "\nVille : " + ville + "\nLibelle : " + libelle +
+                                        "\nNotoriété : " + notoriete;
+                                praticiensDetails.add(details); // Ajouter les détails à la liste
+                                praticiensList.add(nom + " " + prenom);
                             } catch (JSONException e) {
                                 e.printStackTrace();
                                 Toast.makeText(MainActivity.this, "Erreur d'analyse JSON", Toast.LENGTH_SHORT).show();
                             }
                         }
+
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, praticiensList);
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        spinnerDepartements.setAdapter(adapter);
+                        spinnerDepartements.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+                                // Récupérer l'élément sélectionné
+                                String selectedDetails = praticiensDetails.get(position); // Récupérer les détails
+                                // Afficher l'élément sélectionné dans le TextView
+                                textViewDep.setText(selectedDetails); // Afficher les détails
+                            }
+
+                            @Override
+                            public void onNothingSelected(AdapterView<?> parent) {
+                                // Ne rien faire
+                            }
+                        });
                     }
                 },
                 new Response.ErrorListener() {
@@ -124,15 +155,19 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
         requestQueue.add(jsonArrayRequest);
-        return textViewDep;
+        return spinnerDepartements;
     }
-    public TextView fetchNomPraticien(String nomPrat){
-        String url = "https://gsb.siochaptalqper.fr/praticiens/nom/" + nomPrat ;
+
+    public Spinner fetchNomPraticien(String nomPrat) {
+        String url = "https://gsb.siochaptalqper.fr/praticiens/nom/" + nomPrat;
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
                 Request.Method.GET, url, null,
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
+                        ArrayList<String> praticiensList = new ArrayList<>();
+                        ArrayList<String> praticiensDetails = new ArrayList<>(); // Pour stocker les détails
+
                         for (int i = 0; i < response.length(); i++) {
                             try {
                                 JSONObject jsonObject = response.getJSONObject(i);
@@ -144,12 +179,37 @@ public class MainActivity extends AppCompatActivity {
                                 String ville = jsonObject.optString("PRA_VILLE", "Inconnue");
                                 String libelle = jsonObject.optString("TYP_LIBELLE", "Inconnue");
                                 String notoriete = jsonObject.optString("PRA_COEFNOTORIETE", "Inconnue");
-                                textViewNom.append("\n" + "Numéro : " + num + "\n" + "Nom : " + nom + "\n" + "Prénom : " + prenom + "\n" + "Adresse : " + add + "\n" + "Code postal : " + cp+ "\n" + "Ville : " + ville + "\n" + "Libelle :" + libelle + "\n" + "Notoriété : " + notoriete + "\n");
+
+                                // Stocker les détails du praticien
+                                String details = "Numéro : " + num + "\nNom : " + nom + "\nPrénom : " + prenom +
+                                        "\nAdresse : " + add + "\nCode postal : " + cp +
+                                        "\nVille : " + ville + "\nLibelle : " + libelle +
+                                        "\nNotoriété : " + notoriete;
+                                praticiensDetails.add(details); // Ajouter les détails à la liste
+                                praticiensList.add(nom + " " + prenom);
                             } catch (JSONException e) {
                                 e.printStackTrace();
                                 Toast.makeText(MainActivity.this, "Erreur d'analyse JSON", Toast.LENGTH_SHORT).show();
                             }
                         }
+
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, praticiensList);
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        spinnerPraticiens.setAdapter(adapter);
+                        spinnerPraticiens.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+                                // Récupérer l'élément sélectionné
+                                String selectedDetails = praticiensDetails.get(position); // Récupérer les détails
+                                // Afficher l'élément sélectionné dans le TextView
+                                textViewNom.setText(selectedDetails); // Afficher les détails
+                            }
+
+                            @Override
+                            public void onNothingSelected(AdapterView<?> parent) {
+                                // Ne rien faire
+                            }
+                        });
                     }
                 },
                 new Response.ErrorListener() {
@@ -160,7 +220,7 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
         requestQueue.add(jsonArrayRequest);
-        return textViewNom;
+        return spinnerPraticiens;
     }
 
     @Override
@@ -172,7 +232,6 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
         initView();
     }
